e8e44ace8b12e121dfc5ff8dcbbf03a5c6b6590c
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2024-08-28 21:03:38 +0300

    task-3 style done

---
+++ js/task-3.js
@@ -0,0 +1,5 @@
+const headEl = document.querySelector("head");
+const cssLink = document.createElement("link");
+cssLink.rel = "stylesheet";
+cssLink.href = "./css/task-3.css";
+headEl.append(cssLink);
