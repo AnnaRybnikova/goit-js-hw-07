955dca4be2a3e934c9a9d0bc2356781a8b5b011e
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2024-08-29 16:20:16 +0300

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -3,3 +3,12 @@ const cssLink = document.createElement("link");
 cssLink.rel = "stylesheet";
 cssLink.href = "./css/task-3.css";
 headEl.append(cssLink);
+
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", event => {
+    const inputValue = event.currentTarget.value.trim();
+
+    outputEl.textContent = inputValue ? inputValue : "Anonymous";
+})
