ab71682d5557ca0766d1980e0c17db8a9e38090b
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2024-08-29 16:39:02 +0300

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -4,3 +4,22 @@ cssLink.rel = "stylesheet";
 cssLink.href = "./css/task-4.css";
 headEl.append(cssLink);
 
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", event => {
+    event.preventDefault();
+    const formElements = event.target.elements;
+
+    if (!formElements.email.value || !formElements.password.value) {
+        window.alert('All form fields must be filled in');
+    }
+    else {
+        console.log({
+            email: formElements.email.value,
+            password: formElements.password.value,
+        })
+    }
+
+    formElements.email.value = "";
+    formElements.password.value = "";
+})
