46adefc07babd868c20c1614a84b16a9c89a984f
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2024-08-29 16:55:16 +0300

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const bodyEl = document.querySelector("body");
+const colorSpanEl = document.querySelector(".color");
+const changeBtnEl = document.querySelector(".change-color");
+
+changeBtnEl.addEventListener("click", event => {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorSpanEl.textContent = randomColor;
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
