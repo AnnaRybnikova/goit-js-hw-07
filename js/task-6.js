70f69bfd04285b734d2617bcfb2383fafed907bb
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2024-09-04 01:44:56 +0300

    task-6 logic and style done

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,47 @@
+const headEl = document.querySelector("head");
+const cssLink = document.createElement("link");
+cssLink.rel = "stylesheet";
+cssLink.href = "./css/task-6.css";
+headEl.append(cssLink);
+
+const inputEl = document.querySelector("input");
+const createBtnEl = document.querySelector("button[data-create]");
+const destroyBtnEl = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+createBtnEl.addEventListener("click", event => {
+  destroyBoxes()
+  createBoxes(inputEl.value);
+  inputEl.value = "";
+})
+
+destroyBtnEl.addEventListener("click", () => destroyBoxes())
+
+function createBoxes(amount) {
+  let divSize = 30;
+  if (amount >= 1 && amount <= 100) {
+    for (let i = 1; i <= amount; i++) {
+      const divEl = document.createElement("div");
+      if (i !== 1) {
+        divSize += 10;
+      }
+      divEl.style.height = toPixels(divSize);
+      divEl.style.width = toPixels(divSize);
+      divEl.style.backgroundColor = getRandomHexColor();
+
+      boxesEl.append(divEl);
+    }
+  };
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const toPixels = (numb) => `${numb}px`;
